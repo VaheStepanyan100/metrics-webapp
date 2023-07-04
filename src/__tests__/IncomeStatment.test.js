6187f70567dd91f0c2d5942f9f59e6d8c8b33b3c
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-07-04 19:03:19 +0400

    Fix linter error

--- src/__tests__/IncomeStatment.test.js
+++ src/__tests__/IncomeStatment.test.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import IncomeStatement from '../components/incomeStatement';
+import IncomeStatement from '../components/IncomeStatement';
 
 test('renders the income statement correctly', () => {
   const company = [
