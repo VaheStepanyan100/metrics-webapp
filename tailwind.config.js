f699c3db9e1bb9a2e7e0c44fac282658a288ad07
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-07-04 14:15:23 +0400

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js, jsx, ts, tsx}'],
+  theme: {
+    colors: {
+      white: '#ffffff',
+      navbarBg: '#4269b2',
+      headlineBg: '#5688e4',
+      buttonFirstColor: '#4268af',
+      buttonSecondColor: '#3f62a5',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
