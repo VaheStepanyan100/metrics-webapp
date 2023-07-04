4d8beaeab0f1350e02ab1f5bf2cca7c4b88515fe
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-07-04 14:21:56 +0400

    Add routing configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import HomePage from './routes/HomePage';
+import RevenuePage from './routes/RevenuePage';
+import IncomeStatementPage from './routes/IncomeStatementPage';
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/:companyName" element={<RevenuePage />} />
+        <Route path="/:companyName/:year" element={<IncomeStatementPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
